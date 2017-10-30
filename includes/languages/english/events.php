753bb26a1d8273e1f6b45d3c8ea6bad64147d91c
John <dbltoe@users.noreply.github.com>
2017-10-30 15:39:26 -0500

    Add files via upload

---
+++ includes/languages/english/events.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Main Page for Scheduled Events 2.0.0
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: events.php 3001 2017-11-01 11:45:06Z dbltoe $
+ */
+
+define('NAVBAR_TITLE', 'Scheduled Events');
+define('HEADING_TITLE', 'Scheduled Events for ' . STORE_NAME);
+define('TEXT_INFORMATION', '');
+?>
