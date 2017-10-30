f9535270dde34ca4f02dfddab0c1bb83a003cffb
John <dbltoe@users.noreply.github.com>
2017-10-30 16:20:08 -0500

    Add files via upload

---
+++ YOUR_ADMIN/includes/languages/german/extra_definitions/events_manager_menu_name.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package Settings Manager for Scheduled Events 2.0.0
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: events_extra_pages_filenames.php 3001 2017-11-01 11:45:06Z dbltoe $
+ */
+
+// define the Configuration > Scheduled Events Settings
+
+define('BOX_EVENTS_MANAGER', 'Scheduled Events Manager');
