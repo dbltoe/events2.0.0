6a10c74e6ded1d825393cfe562c78d26df5a5404
John <dbltoe@users.noreply.github.com>
2017-10-30 15:38:21 -0500

    Add files via upload

---
+++ includes/extra_datafiles/events_extra_pages_filenames.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Information Box for Scheduled Events 2.0.0
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: events_extra_pages_filenames.php 3001 2017-11-01 11:45:06Z dbltoe $
+ */
+
+// define the extra page filenames
+
+  define('FILENAME_DEFINE_EVENTS', 'define_events');
+  define('FILENAME_EVENTS', 'events');
+?>
---
+++ includes/extra_datafiles/events_filenames.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package Information Box for Scheduled Events 2.0.0
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: events_filenames.php 3001 2017-11-01 11:45:06Z dbltoe $
+ */
+
+	// information box text in sideboxes/information.php for Scheduled Event
+	define('BOX_INFORMATION_EVENTS', 'Scheduled Events');
+?>
